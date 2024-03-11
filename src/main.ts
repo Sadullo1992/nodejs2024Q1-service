65228811e6c102011bed806a4f1399d37df01ac0
Sadullo <sadulloburiyev1992@gmail.com>
2024-03-11 14:03:34 +0500

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { yamlFileLoader } from './helpers/yamlFileLoader';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
+
   const app = await NestFactory.create(AppModule);
+
+  // Validation
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,7 +17,11 @@ async function bootstrap() {
       forbidUnknownValues: true,
     }),
   );
-  console.log('App listened port: ', PORT);
+
+  // Swagger config
+  const document = (await yamlFileLoader('./doc/api.yaml')) as OpenAPIObject;
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
